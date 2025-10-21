715818287b27ae56d3930e2138fd0116cab8090a
ZaneG528 <zanegibson528@gmail.com>
2025-10-21 08:57:12 -0700

    Cat
    ASCII of a cat

---
+++ Assignment102025.java
@@ -0,0 +1,14 @@
+public class Assignment102025{
+   public static void main(String args[]){
+
+      System.out.println(")\\)\\");
+      System.out.println(").. \\");
+      System.out.println("\\4_, '-.");
+      System.out.println("  )     '.");
+      System.out.println("  |  \\/   \\");
+      System.out.println("  \\\\ |\\_  |_");
+      System.out.println("  ((_/(__/_,'.");
+      System.out.println("       (,----');");
+   }
+
+}
